79ad8312076449694e7da32be2cc96aee58a40e2
Piddubny <71131374+Piddubny@users.noreply.github.com>
2022-02-09 18:48:05 +0200

    test
    test

--- wdio.conf.js
+++ wdio.conf.js
@@ -271,3 +271,9 @@ exports.config = {
     //onReload: function(oldSessionId, newSessionId) {
     //}
 }
+if (process.env.CI == 'true') {
+    configToExport.logLevel = 'error'
+    configToExport.services = configToExport.services.filter(service => service !== 'chromedriver')
+    configToExport.hostname = 'localhost'
+    configToExport.path = '/wd/hub'
+}
